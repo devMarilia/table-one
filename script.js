5f263803c5535e44c35b4c5b59ebd1fd0273d050
devMarilia <devMarilia.frontend@gmail.com>
2022-06-18 15:06:17 -0300

    ✨ feat: varification create user exists

--- script.js
+++ script.js
@@ -1,38 +1,56 @@
 function cadPessoa(nome, sobrenome, email, ra) {
-  var tb = document.getElementById('table');
-  var qtdLine = tb.rows.length;
-  var line = tb.insertRow(qtdLine);
-  var msgError = document.getElementById('msgError');
+  let ifExistePessoa = [];
+  let tb = document.getElementById('table');
+  let qtdLine = tb.rows.length;
+  let line = tb.insertRow(qtdLine);
+  let msgError = document.getElementById('msgError');
+  let Rows = document.querySelectorAll('#table > tbody > tr > td');
+
+  Array.from(Rows).forEach((element) => { // verifica se ja existe a pessoa
+    if (element.innerHTML === sobrenome) { // se ja existe a pessoa
+      ifExistePessoa.push(element); // armazena a pessoa
+    } else if (element.innerHTML === email) { // se ja existe o email
+      ifExistePessoa.push(element); // armazena o email
+    } else if (element.innerHTML === ra) { // se ja existe o ra
+      ifExistePessoa.push(element); //  armazena o ra
+    } else { // se nao existe a pessoa
+      msgError.innerHTML = 'Pessoa já cadastrada'; // mostra mensagem de erro
+    }
+  });
+  
 
   if(nome === "" || sobrenome === "" || email === "" || ra === "") { // Verifica se os campos estão vazios
     msgError.innerHTML = "Preencha todos os campos!"; // Exibe mensagem de erro
     return;
-  } else if (ra.length !== 8 || ra.length < 8) { // Verifica se o RA tem 8 caracteres
+  } else if (ra.length !== 8 ) { // Verifica se o RA tem 8 caracteres
     msgError.innerHTML = "RA deve conter 8 caracteres!";  
     return;
   } else if (email.indexOf('@') === -1) { // verifica se existe o @
     msgError.innerHTML = "Email inválido!"; //se não existir, mostra mensagem de erro
     return; // se não existir, retorna
-  } if (email.indexOf('.') === -1) { // verifica se existe o .
+  } else if (email.indexOf('.') === -1) { // verifica se existe o .
     msgError.innerHTML = "Email inválido!"; //se não existir, mostra mensagem de erro
     return; // se não existir, retorna
-  }
-  else {
+  } else if (ifExistePessoa.length > 0) { // verifica se existe alguma pessoa com o mesmo nome, sobrenome, email ou ra
+    msgError.innerHTML = "Pessoa já cadastrada!"; //se existir, mostra mensagem de erro
+    return; // se existir, retorna
+  } else {
     msgError.innerHTML = "";
   }
  
-  var cellName = line.insertCell(0);
-  var cellSobrenome = line.insertCell(1);
-  var cellEmail = line.insertCell(2);
-  var cellRa = line.insertCell(3);
+  let cellName = line.insertCell(0);
+  let cellSobrenome = line.insertCell(1);
+  let cellEmail = line.insertCell(2);
+  let cellRa = line.insertCell(3);
  
   cellName.innerHTML = nome;
   cellSobrenome.innerHTML = sobrenome;
   cellEmail.innerHTML = email;
   cellRa.innerHTML = ra;
 
-  var btn = document.createElement('button');
+  let btn = document.createElement('button');
     
  }
+ 
 
  
