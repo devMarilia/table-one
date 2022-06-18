bfabf02da5f18b05c69b78455c08ca3d242301f3
devMarilia <devMarilia.frontend@gmail.com>
2022-06-18 12:16:50 -0300

    ✨ feat: validation in form

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ function cadPessoa(nome, sobrenome, email, ra) {
   var tb = document.getElementById('table');
   var qtdLine = tb.rows.length;
   var line = tb.insertRow(qtdLine);
+  var msgError = document.getElementById('msgError');
+
+  if(nome === "" || sobrenome === "" || email === "" || ra === "") { // Verifica se os campos estão vazios
+    msgError.innerHTML = "Preencha todos os campos!"; // Exibe mensagem de erro
+    return;
+  } else if (ra.length !== 8 || ra.length < 8) { // Verifica se o RA tem 8 caracteres
+    msgError.innerHTML = "RA deve conter 8 caracteres!";  
+    return;
+  } else if (email.indexOf('@') === -1) { // verifica se existe o @
+    msgError.innerHTML = "Email inválido!"; //se não existir, mostra mensagem de erro
+    return; // se não existir, retorna
+  } if (email.indexOf('.') === -1) { // verifica se existe o .
+    msgError.innerHTML = "Email inválido!"; //se não existir, mostra mensagem de erro
+    return; // se não existir, retorna
+  }
+  else {
+    msgError.innerHTML = "";
+  }
  
   var cellName = line.insertCell(0);
   var cellSobrenome = line.insertCell(1);
@@ -16,3 +34,5 @@ function cadPessoa(nome, sobrenome, email, ra) {
   var btn = document.createElement('button');
     
  }
+
+ 
